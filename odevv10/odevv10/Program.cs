60d4f52615721549a2c5db8049b639291367cd0b
Hilal Gül Arıboğa <150022719+hilalariboga34@users.noreply.github.com>
2024-10-29 21:37:58 +0300

    Add files via upload

---
+++ odevv10/odevv10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Geçerli işlemi kontrol eden fonksiyon
+    static bool IslemGecerli(double sonuc, char op, double num)
+    {
+        if (op == '+') return sonuc + num > 0;
+        if (op == '-') return sonuc - num > 0;
+        if (op == '*') return sonuc * num > 0;
+        if (op == '/' && num != 0) return sonuc / num > 0;
+        return false;
+    }
+
+    // Operasyonları gerçekleştirerek tapınağın kapısını açan kombinasyonu bulmaya çalışır
+    static void KapıyıAcma(List<double> sayilar, List<char> islemler, int index, double sonuc)
+    {
+        if (index == sayilar.Count)
+        {
+            if (sonuc > 0) Console.WriteLine("Kombinasyon bulundu, sonuç: " + sonuc);
+            return;
+        }
+
+        foreach (char op in islemler)
+        {
+            if (IslemGecerli(sonuc, op, sayilar[index]))
+            {
+                double yeniSonuc = 0;
+                switch (op)
+                {
+                    case '+':
+                        yeniSonuc = sonuc + sayilar[index];
+                        break;
+                    case '-':
+                        yeniSonuc = sonuc - sayilar[index];
+                        break;
+                    case '*':
+                        yeniSonuc = sonuc * sayilar[index];
+                        break;
+                    case '/':
+                        if (sayilar[index] != 0)
+                            yeniSonuc = sonuc / sayilar[index];
+                        break;
+                }
+
+                KapıyıAcma(sayilar, islemler, index + 1, yeniSonuc);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        // Örnek sayılar ve işlemler
+        List<double> sayilar = new List<double> { 5, 3, 2 };
+        List<char> islemler = new List<char> { '+', '-', '*', '/' };
+
+        // İlk sayıyı başlangıç değeri olarak kullanıyoruz
+        double baslangicSonuc = sayilar[0];
+
+        Console.WriteLine("Geçerli kombinasyonlar:");
+        KapıyıAcma(sayilar, islemler, 1, baslangicSonuc);
+    }
+}
---
+++ odevv9/odevv9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static int EnDusukEnerjiMaliyeti(int[,] enerji, int N)
+    {
+        int[,] dp = new int[N, N];
+        dp[0, 0] = enerji[0, 0];
+
+        // İlk satırdaki hücrelerin maliyetlerini hesapla (sadece sağa hareket edilebilir)
+        for (int j = 1; j < N; j++)
+            dp[0, j] = dp[0, j - 1] + enerji[0, j];
+
+        // İlk sütundaki hücrelerin maliyetlerini hesapla (sadece aşağı hareket edilebilir)
+        for (int i = 1; i < N; i++)
+            dp[i, 0] = dp[i - 1, 0] + enerji[i, 0];
+
+        // Diğer hücreler için sağa, aşağıya veya sağ aşağı diyagonal hareketleri değerlendir
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = 1; j < N; j++)
+            {
+                dp[i, j] = enerji[i, j] + Math.Min(dp[i - 1, j], Math.Min(dp[i, j - 1], dp[i - 1, j - 1]));
+            }
+        }
+
+        // Hedef noktadaki (N-1, N-1) minimum enerji maliyeti
+        return dp[N - 1, N - 1];
+    }
+
+    static void Main()
+    {
+        // Enerji matrisi örneği
+        int[,] enerji = {
+            { 4, 7, 8, 6, 4 },
+            { 6, 7, 3, 9, 2 },
+            { 3, 8, 1, 2, 4 },
+            { 7, 1, 7, 3, 7 },
+            { 2, 9, 8, 9, 3 }
+        };
+
+        int N = enerji.GetLength(0);
+        int sonuc = EnDusukEnerjiMaliyeti(enerji, N);
+
+        Console.WriteLine("En az enerji maliyeti: " + sonuc);
+    }
+}
